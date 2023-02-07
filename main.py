66ffbc44626dcdb728231dd447a9d08009e33dd4
sinlnw <sinlnw4567@gmail.com>
2023-02-07 10:05:45 +0700

    gogo

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("hello")
-print("world")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]# allow everyone
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
